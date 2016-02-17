b8a2491401b8ceb6a06415b9cb57043dab721a87
frechop <dkarapetyan94@gmail.com>
2016-02-17 19:31:35 +0400

    Initial Commit

---
+++ Lesson 02/MVC_HelloWorld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_HelloWorld.Startup))]
+namespace MVC_HelloWorld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
